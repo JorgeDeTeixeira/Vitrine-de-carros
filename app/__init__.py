40a0c0e439528e200e9257cf93335e987793ce98
Jorge Teixeira <jorgedeteixeirademelo@gmail.com>
2024-01-17 08:50:57 -0300

    Projeto com crud completo

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from decouple import config
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = '123456'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:123456@localhost:3306/cars'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = config('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = config('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = config(
+    'SQLALCHEMY_TRACK_MODIFICATIONS')
 
 db = SQLAlchemy(app)
 
--- app/forms.py
+++ app/forms.py
@@ -3,11 +3,14 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, FloatField, SubmitField
 from wtforms.validators import DataRequired, NumberRange
 
+
 class CarroForm(FlaskForm):
     marca = StringField('Marca', validators=[DataRequired()])
     modelo = StringField('Modelo', validators=[DataRequired()])
     nome = StringField('Nome', validators=[DataRequired()])
-    ano = IntegerField('Ano', validators=[DataRequired(), NumberRange(min=1800, max=3000)])
-    preco = FloatField('Preço', validators=[DataRequired(), NumberRange(min=0)])
+    ano = IntegerField('Ano', validators=[
+                       DataRequired(), NumberRange(min=1800, max=3000)])
+    preco = FloatField('Preço', validators=[
+                       DataRequired(), NumberRange(min=0)])
     img = StringField('URL da Imagem', validators=[DataRequired()])
     submit = SubmitField('Adicionar Carro')
